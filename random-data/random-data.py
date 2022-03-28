88f16218fde3eb306f13d03ea0b550f62b788db5
aaelony <aaelony@gmail.com>
2022-03-28 14:55:01 -0700

    and dates...

--- random-data/random-data.py
+++ random-data/random-data.py
@@ -11,6 +11,7 @@ def random_df(nrows=5):
         {'gender': [np.random.choice(['M','F']) for i in range(nrows)],
          'colors': [np.random.choice(['red','orange', 'yellow','green','blue','indigo', 'violet']) for i in range(nrows)],
          'age' : np.random.randint(low=18, high=80, size= nrows, dtype=int),
+         'visit_date' : [np.random.choice(pd.date_range(start='2020-01-01', end=date.today() )) for i in range(nrows)],
          'visits' : np.random.default_rng().poisson(lam=1.0, size=nrows),
          'paidamt' : np.random.default_rng().normal(loc=100.0, scale=90.0, size=nrows).round(decimals=2)
         })
